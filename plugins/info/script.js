8ce57f7f1c5bdf523bfa09b2e16990695bf508c9
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-04-09 11:16:18 +0700

    Create script.js

---
+++ plugins/info/script.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const pkg = require(process.cwd() + "/package.json")
+
+module.exports = {
+    help: ["script", "sc", "scbot"].map(v => v + ' *[ Melihat Info Script Bot ]* '),
+    tags: ["info"],
+    command: ["script", "sc", "scbot"],
+    code: async (m, {
+        conn,
+        Func,
+        config
+    }) => {
+
+        let data = await axios
+            .get("https://api.github.com/repos/LeooxzyDekuu/Rin-Base-Simple")
+            .then((a) => a.data);
+
+        let cap = "*– 乂 Informasi - Script Bot*\n\n";
+        cap += `> 🧩 *Nama:* ${data.name}\n`;
+        cap += `> 👤 *Pemilik:* ${data.owner.login}\n`;
+        cap += `> ⭐ *Star:* ${data.stargazers_count}\n`;
+        cap += `> ℹ️ *Version:* ${pkg.version}\n`
+    cap += `> 🍴 *Forks:* ${data.forks}\n`;
+        cap += `> 📅 *Dibuat sejak:* ${Func.ago(data.created_at)}\n`;
+        cap += `> 🔄 *Terakhir Update:* ${Func.ago(data.updated_at)}\n`;
+        cap += `> 🔄 *Terakhir Publish:* ${Func.ago(data.pushed_at)}\n`;
+        cap += `> 🔗 *Link Repository:* ${data.html_url}\n\n`;
+        cap +=
+            "🔧 *Fitur Utama Script Bot:*\n" +
+            "> ✅ *Support Case x Plugins*\n" +
+            "> ✅ *Ukuran Script Ringan*\n" +
+            "> ✅ *100% Menggunakan Scrape*\n" +
+            "> ✅ *Respon Polling & Edit*\n" +
+            "> ✅ *Auto Reload File Scrape*\n" +
+            "> ✅ *Support Run Di Mana Saja*\n\n";
+        cap +=
+            "Script ini gratis, boleh kalian recode dan jual asal jangan hapus credit original dari kami!";
+
+        m.reply(cap);
+    }
+};
