39b58a75fc292edc1690273a8f7c37cc06bd9252
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-04-19 06:39:22 +0700

    Update ytdl.js

--- plugins/downloader/ytdl.js
+++ plugins/downloader/ytdl.js
@@ -1,11 +1,20 @@
+// © HanakoBotz
+// • By: Leooxzy - Deku
+// • Owner: 6283136099660
+
+// By: Leooxzy
+// Bio cr: Krz
+
 let yukio = async (m, {
     conn,
     text,
     Scraper,
+    Uploader,
     Func
 }) => {
     switch (m.command) {
-        case 'ytmp4': {
+        case 'ytmp4':
+        case 'ytv': {
             if (!text.includes('youtu')) throw '⚠️Masukan Masukan Link Yt !'
             const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=|shorts\/))([^\?&]+)(?:\?is=[^&]*)?(?:\?si=[^&]*)?(?:&.*)?/;
             const videoId = text.match(regex);
@@ -38,19 +47,34 @@ let yukio = async (m, {
             }, {
                 quoted: m
             });
-            const ytdl = await Scraper.amdl(text, 'video', '720p');
-            const size = await Func.getSize(ytdl.download);
-            conn.sendMessage(m.chat, {
-                video: {
-                    url: ytdl.download
-                },
-                caption: 'Title: ' + result.title,
-            }, {
-                quoted: m
+            const ytdl = await Scraper.savetube.download(result.url, '720');
+            const getArray = await axios.get(ytdl.result.download, {
+                responseType: 'arraybuffer'
             });
+            if (size > 100 * 1024 * 1024) {
+                await sock.sendMessage(m.cht, {
+                    document: {
+                        url: ytdl.result.download
+                    },
+                    mimetype: "audio/mpeg",
+                    fileName: `${result.title}.mp3`,
+                }, {
+                    quoted: m
+                });
+            } else {
+                conn.sendMessage(m.chat, {
+                    video: {
+                        url: ytdl.result.download
+                    },
+                    caption: 'Title: ' + result.title,
+                }, {
+                    quoted: m
+                });
+            }
         }
         break;
-        case 'ytmp3': {
+        case 'ytmp3':
+        case 'yta': {
             if (!text.includes('youtu')) throw '⚠️Masukan Masukan Link Yt !';
             const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=|shorts\/))([^\?&]+)(?:\?is=[^&]*)?(?:\?si=[^&]*)?(?:&.*)?/;
             const videoId = text.match(regex);
@@ -83,34 +107,50 @@ let yukio = async (m, {
             }, {
                 quoted: m
             });
-            const ytdl = await Scraper.amdl(text, 'audio', '320k')
-            const size = await Func.getSize(ytdl.download);
-            conn.sendMessage(m.chat, {
-                audio: {
-                    url: ytdl.download
-                },
-                mimetype: 'audio/mpeg',
-                contextInfo: {
-                    isForwarded: true,
-                    forwardingScore: 99999,
-                    externalAdReply: {
-                        title: result.title,
-                        body: result.timestamp + ' / ' + size + ' / ' + ytdl.format,
-                        mediaType: 1,
-                        thumbnailUrl: result.thumbnail,
-                        renderLargerThumbnail: false,
-                        sourceUrl: result.url
-                    }
-                }
-            }, {
-                quoted: m
+            const ytdl = await Scraper.savetube.download(result.url, 'mp3');
+            const getArray = await axios.get(ytdl.result.download, {
+                responseType: 'arraybuffer'
             });
+            const tmp = await Uploader.tmpfiles(getArray.data);
+            const size = await Func.getSize(tmp);
+            if (getArray > 100 * 1024 * 1024) {
+                await sock.sendMessage(m.cht, {
+                    document: {
+                        url: ytdl.result.download
+                    },
+                    mimetype: "audio/mpeg",
+                    fileName: `${result.title}.mp3`,
+                }, {
+                    quoted: m
+                });
+            } else {
+                conn.sendMessage(m.chat, {
+                    audio: {
+                        url: ytdl.result.download
+                    },
+                    mimetype: 'audio/mpeg',
+                    contextInfo: {
+                        isForwarded: true,
+                        forwardingScore: 99999,
+                        externalAdReply: {
+                            title: result.title,
+                            body: result.timestamp + ' / ' + size + ' / ' + ytdl.result.format,
+                            mediaType: 1,
+                            thumbnailUrl: result.thumbnail,
+                            renderLargerThumbnail: false,
+                            sourceUrl: result.url
+                        }
+                    }
+                }, {
+                    quoted: m
+                });
+            }
         }
         break;
     }
 };
 
-yukio.help = ["ytmp3", "ytmp4"].map(v => v + ' *[ Request Lagu Yt Yg Pengen Di Putar ]* ');
+yukio.help = ["ytmp3", "ytmp4", "yta", "ytv"].map(v => v + ' *[ Request Lagu Yt Yg Pengen Di Putar ]* ');
 yukio.tags = ["downloader"];
 yukio.command = ["ytmp3", "ytmp4"];
 yukio.loading = true;
